244d77f9ca044f00a2cf9f5d9962204b44287957
Ubaid-Mirza <ubaids6o5e7@gmail.com>
2023-01-06 16:57:48 +0500

    make footer and some other changes and responsive the landing page.

---
+++ components/navbaar-i.php
@@ -0,0 +1,27 @@
+<!-- START OF NAVBAAR I-->
+<div class="container-fluid navbaar-i">
+
+
+<a class="navbar-brand" href="#">
+    <img src="logo/logo.svg" alt="" width="130" height="50">
+</a>
+
+
+
+<ul class="nav nav-list-i">
+    <li class="nav-item one">
+        <a class="nav-link nav-list-i-a" aria-current="page" href="#">Forums</a>
+    </li>
+    <li class="nav-item one">
+        <a class="nav-link nav-list-i-a" href="#">Start Selling</a>
+    </li>
+    <li class="nav-item one">
+        <a class="nav-link nav-list-i-a" href="#">Our Products</a>
+    </li>
+    <li class="nav-item one">
+        <a class="nav-link nav-list-i-a" href="#" tabindex="-1" aria-disabled="true">Sign In</a>
+    </li>
+</ul>
+</div>
+
+<!-- END OF NAVBAAR I-->
